2d1e10e97f841b65301bacaecbf6cdccf8d077d0
Max McKenzie <max@lemondigits.com>
2016-06-14 17:50:22 +0100

    framework for mocha set up

--- entrypoint.js
+++ entrypoint.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
 
+require('babel-register');
 require('./dist/index.js');
--- test/install-spec.js
+++ test/install-spec.js
@@ -1,6 +1,6 @@
-import app from './setup';
+import install from '../dist/commands/install';
 
-describe('install',() => {
+describe('install', () => {
   it('returns a console.log of the framework choosen', (done) => {
     
   });
