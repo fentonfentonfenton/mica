ca9dcbb3d0eb8c867b1f6045018bd6c03c569d89
Max McKenzie <max@lemondigits.com>
2016-06-16 13:17:46 +0100

    update to the naming of commands

--- app/commands/install.js
+++ app/commands/install.js
@@ -6,13 +6,13 @@ import webdriverio from '../helpers/webdriverio';
 export function install(framework) {
   switch (framework) {
     default:
-      logger.error('please select a framework (capybara|webdriver|angular)');
+      logger.error('please select a framework (capybara|webdriverio|webdriverio-angular)');
       break;
     case 'capybara':
       logger.info('capybara');
       break;
     case 'webdriverio':
-      logger.info('installing webdriverio');
+      logger.info('installing webdriverio'.green);
       webdriverio();
       break;
   }
